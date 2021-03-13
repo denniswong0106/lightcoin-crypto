c8a0d53c3e8b2c97332cb7c597a90ac1cd04c54f
Dennis Wong <denniswong0106@gmail.com>
2021-03-13 03:48:30 +0000

    refactored Deposit and Withdrawal to be subclass of transactions. Added class account to create account specific balances

--- index.js
+++ index.js
@@ -1,29 +1,53 @@
-let balance = 500.00;
+class Account {
+  constructor (username) {
+    this.username = username;
 
-class Withdrawal {
+    this.balance = 0;
+  }
+};
 
-  constructor(amount) {
+class Transaction {
+
+  constructor(amount, account) {
     this.amount = amount;
+    this.account = account;
+  }
+};
+
+
+class Withdrawal extends Transaction {
+
+  get value() {
+    return -(this.amount);
   }
 
-  commit() {
-    balance -= this.amount;
+  // commit() {
+  //   this.account.balance -= this.amount;
+  // }
+};
+
+class Deposit extends Transaction {
+
+  get value() {
+    return
   }
 
-}
+  // commit() {
+  //   this.account.balance += this.amount;
+  // }
+};
 
 
 
+// DRIVER CODE
 
-// DRIVER CODE BELOW
-// We use the code below to "drive" the application logic above and make sure it's working as expected
-
-t1 = new Withdrawal(50.25);
+const myAccount = new Account("snow-patrol");
+t1 = new Deposit(500, myAccount);
 t1.commit();
 console.log('Transaction 1:', t1);
 
-t2 = new Withdrawal(9.99);
+t2 = new Withdrawal(50.25, myAccount);
 t2.commit();
 console.log('Transaction 2:', t2);
 
-console.log('Balance:', balance);
+console.log(myAccount.balance)
